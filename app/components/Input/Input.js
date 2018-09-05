8723040ec25feec8ff9a73982a065f57b60afeca
Ashish Patel <ashishpatel0720@gmail.com>
2018-09-06 00:03:03 +0530

    UI<input.js>: updated, fonts added for both fields

--- app/components/Input/Input.js
+++ app/components/Input/Input.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 const Input=()=>{
+  const backgroundColor='#403759';
+
   const style1={
-    minHeight:'33%'
+    minHeight:'33%',
+    backgroundColor
   };
   const style2={
     border:'none',
-    backgroundColor:'#403759'
+    backgroundColor
   };
+  const style3={
+    border:'none',
+    backgroundColor,
+  };
+
   return (
-    <div className="field flex flex-nowrap" style={style1}>
-      <input type="text" style={style2} className="min-w-full min-h-full"/>
+    <div className="field flex flex-wrap" style={style1}>
+      <div className="font-mono  text-white text-2xl min-w-full">Heading</div>
+      <input type="text" style={style2}
+             className=" font-mono text-right  text-white text-2xl min-w-full" value="1334*(3343+232342)"/>
+      <div style={style3}
+           className='font-mono text-right text-white text-4xl min-w-full'>=asdkfjdskjfds
+      </div>
     </div>
     )
 };
